5d51e8bc32e061851a318675a4acb6b552225b9b
HomeScrazy <crazy_runcheng@163.com>
2015-02-05 13:47:25 +0800

    little change

---
+++ admin/adminlogin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+<head>
+  <title>后台-管理员登录</title>
+  <script src="..\dist\js\bootstrap.js"></script>
+  <link rel="stylesheet" type="text/css" href="..\dist\css\bootstrap.css">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<div class="container">
+  <div class="row-fluid">
+  	<div class="span4 offset9"><h1>管理员登录</h1></div>
+  </div>
+</div>
+</body>
+</html>
